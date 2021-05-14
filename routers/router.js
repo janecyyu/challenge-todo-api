87e5a8312944743fed2c4481f97840fd1984b6c4
janecyyu <janecyyu@gmail.com>
2021-05-13 20:19:45 -0700

    all access allow

--- routers/router.js
+++ routers/router.js
@@ -8,6 +8,7 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   db.add(req.body)
     .then((data) => res.status(201).json({ message: data }))
     .catch((err) => {
@@ -17,6 +18,7 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   db.update(req.params.id, req.body)
     .then(() => {
       res.status(200).json("update successfully");
@@ -28,6 +30,7 @@ router.put("/:id", (req, res) => {
 });
 
 router.delete("/:id", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
   db.remove(req.params.id)
     .then(() => {
       res.status(200).json("remove successfully");
